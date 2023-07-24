217b29a1175216508ba17bcd796d54de4356cdd7
joyyg0606 <joyyg0606@gmail.com>
2023-07-24 11:51:58 -0400

    snake game

---
+++ SNAKE GAME/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 275)
+        self.write(f"Score: {self.score}", align='center', font=('Courier', 12, 'normal'))
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align='center', font=('Courier', 12, 'normal'))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align='center', font=('Courier', 20, 'normal'))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+
+        
