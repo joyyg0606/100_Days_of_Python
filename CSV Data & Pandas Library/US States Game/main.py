6b37c812466aacfb00107c1924beff469f2c5b35
joyyg0606 <joyyg0606@gmail.com>
2023-07-28 17:25:33 -0400

    CSV data and Pandas

---
+++ CSV Data & Pandas Library/US States Game/main.py
@@ -0,0 +1,33 @@
+import pandas, csv, turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "CSV Data & Pandas Library/US States Game/blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("CSV Data & Pandas Library/US States Game/50_states.csv")
+state_list = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Guessed", prompt="Enter a state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in state_list:
+            if state not in guessed_states:
+                missing_states.append(state)
+
+        data = pandas.DataFrame(missing_states)
+        data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in state_list:
+        guessed_states.append(answer_state)
+        text = turtle.Turtle()
+        text.penup()
+        text.hideturtle()
+        current_state_data = data[data.state == answer_state]
+        text.goto(int(current_state_data.x), int(current_state_data.y))
+        text.write(answer_state, align='center')
---
+++ CSV Data & Pandas Library/main.py
@@ -0,0 +1,64 @@
+import pandas, csv
+
+data = pandas.read_csv("CSV Data & Pandas Library/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_count = len(data[data["Primary Fur Color"] == "Gray"])
+cin_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Black", "Cinnamon"],
+    "Count": [gray_count, black_count, cin_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+
+with open("CSV Data & Pandas Library/weather_data.csv") as data_file:
+    data = data_file.readlines()
+    print(data)
+
+with open("CSV Data & Pandas Library/weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != 'temp':
+            temperatures.append(int(row[1]))
+
+    print(temperatures)
+
+data = pandas.read_csv("CSV Data & Pandas Library/weather_data.csv")
+print(data['temp'])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+print(data['temp'].to_list())
+
+avrg_temp = sum(temp_list) / len(temp_list)
+print(avrg_temp)
+    
+print(data['temp'].mean())
+
+print(data['temp'].max())
+
+print(data.condition)
+
+# Get data in row
+print(data[data.day == 'Monday'])
+
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == 'Monday']
+
+monday_temp = int(monday.temp)
+monday_temp_f = monday_temp * 1.8 + 32
+
+print(f"{monday_temp_f}°F")
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["a", "b", "c"],
+    "scores": [1, 2, 3]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
