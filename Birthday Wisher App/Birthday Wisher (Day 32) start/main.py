f7057d7199f4bb4e1f0674fe7a479613c6fbd17f
joyyg0606 <joyyg0606@gmail.com>
2023-08-08 10:26:14 -0400

    bday wisher edits

--- Birthday Wisher App/Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher App/Birthday Wisher (Day 32) start/main.py
@@ -1,9 +1,9 @@
 import smtplib
 
-my_email = "liv125461@gmail.com"
+my_email = "example@gmail.com"
 password = "abc1234"
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="vxntcy@gmail.com", msg="Subject: Hello\n\nHello")
+connection.sendmail(from_addr=my_email, to_addrs="2example@gmail.com", msg="Subject: Hello\n\nHello")
 connection.close()
