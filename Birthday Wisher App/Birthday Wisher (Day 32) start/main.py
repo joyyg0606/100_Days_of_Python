2bf021658f39bab8b7975df1b05d7359852202bc
joyyg0606 <joyyg0606@gmail.com>
2023-08-08 10:22:56 -0400

    flasy cards

--- Birthday Wisher App/Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher App/Birthday Wisher (Day 32) start/main.py
@@ -1,7 +1,7 @@
 import smtplib
 
 my_email = "liv125461@gmail.com"
-password = "abc123"
+password = "abc1234"
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls(user=my_email, password=password)
